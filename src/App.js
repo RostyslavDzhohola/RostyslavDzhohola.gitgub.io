3f44cfcb21fe9ff3a4e36c95cef88bd373f7c892
Rostyslav <dg1h1t@protonmail.com>
2022-05-18 17:52:58 -0500

    Changing App function to a Component

--- src/App.js
+++ src/App.js
@@ -2,39 +2,77 @@ import './App.css';
 import Coin from './components/Coin/Coin';
 import AccountBalance from './components/AccountBalance/AccountBalance';
 import logo from './logo.svg';
+import React from 'react'
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <img src={logo} alt="React logo" className="App-logo"
-       />
-        <h1 className="App-title">
-          Coin Exchange 
-        </h1>
-     
-      </header>
-      <AccountBalance amount={10000}/>
-      <table className="coin-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Ticker</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Coin name="Bitcoin" ticker="BTC" price={45000} />
-          <Coin name="Ethereum" ticker="ETH" price={200} />
-          <Coin name="Dogecoin" ticker="DOGE" price={0.003} />
-          <Coin name="Tether" ticker ="USDT" price={1.0}/>
-          <Coin name="Ripple" ticker="XRP" price={0.4}/>
-        </tbody>
-      </table>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: 20000,
+      coinData: [
+        {
+          name: "Bitcoin", 
+          ticker: "BTC",
+          price: 29999.99
+        },
+        {
+          name: "Ethereum", 
+          ticker: "ETH",
+          price: 1900
+        },
+        {
+          name: "Dogecoin", 
+          ticker: "DOGE",
+          price: 0.003
+        },
+        {
+          name: "Tether", 
+          ticker: "USDT",
+          price: 1.0
+        },
+        {
+          name: "Solana", 
+          ticker: "SOL",
+          price: 50
+        }
 
-    </div>
-  );
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         <img src={logo} alt="React logo" className="App-logo"/>
+          <h1 className="App-title">
+            Coin Exchange 
+          </h1>
+       
+        </header>
+        <AccountBalance amount={this.state.balance}/>
+        <table className="coin-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Ticker</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            { 
+              this.state.coinData.map(v => 
+                <Coin key={v.ticker} {...v}/>
+              )
+            }
+          </tbody>
+        </table>
+  
+      </div>
+    );
+  }
+  
 }
 
 export default App;
